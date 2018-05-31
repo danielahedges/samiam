6c68dab645b0414f886d4e0ec3862bb00b49a72e
Dan Hedges <dhedges@epatientfinder.com>
2018-05-31 10:02:41 -0500

    changed db name to reflect rebranding

--- src/core/server/config/env/development.env.config.js
+++ src/core/server/config/env/development.env.config.js
@@ -2,7 +2,7 @@ const url = process.env.APP_URL || 'http://localhost:6005';
 
 module.exports = {
   name: 'dev',
-  db: 'mongodb://localhost/seed2',
+  db: 'mongodb://localhost/monocot',
   sessionSecret: 'sprawledCounterfeitCountersignedLozenge',
   facebook: {
     enabled: !!process.env.FACEBOOK_OAUTH_ENABLED,
